97270c4fcf0b1800382b4bd2b470981b113ac634
0KateloK0 <katelkin2003@mail.ru>
2024-11-16 17:37:12 +0300

    added tyxml

---
+++ lib/thtml.ml
@@ -0,0 +1,2 @@
+open Tyxml
+let to_ocaml = Html.(a ~a:[a_href "ocaml.org"] [txt "OCaml!"])
---
+++ test/cover_tests/ehtml_test.ml
@@ -0,0 +1,9 @@
+(* let () =
+ assert (Ehtml.render "word" =
+"<html>
+<body>
+  <h1>The URL parameter was word!</h1>
+</body>
+</html>\n\n\n") *)
+
+let () = assert(true)

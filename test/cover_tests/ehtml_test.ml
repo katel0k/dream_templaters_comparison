c3a094416a9cf0826db0a7cf8ab1d4385393c5c2
0KateloK0 <katelkin2003@mail.ru>
2024-11-20 13:27:38 +0300

    made eml test more interesting

--- test/cover_tests/ehtml_test.ml
+++ test/cover_tests/ehtml_test.ml
@@ -4,34 +4,35 @@ let () =
 <head>
     <title>My comparison test</title>
     <style>
-        .wrapper {
-            background-color: grey;
-            padding: 100px 20% 100px 20%;
-        }
-        #padding {
-            font-size: 20px;
-            color: lightblue;
-        }
-        body {
-            margin: 0;
-        }
-        .repeating_values_wrapper {
-            display: flex;
-            justify-content: space-evenly;
-        }
-        .repeating_value {
-            width: 100%;
-            text-align: center;
-            display: flex;
-            justify-content: space-around;
-            flex-direction: column;
-            vertical-align: middle;
-        }
-        .repeating_values-colored {
-            background-color: grey;
-            height: 400px;
-        }
-    </style>
+  .wrapper {
+      background-color: grey;
+      padding: 100px 20% 100px 20%;
+  }
+  #padding {
+      font-size: 20px;
+      color: lightblue;
+  }
+  body {
+      margin: 0;
+  }
+  .repeating_values_wrapper {
+      display: flex;
+      justify-content: space-evenly;
+  }
+  .repeating_value {
+      width: 100%;
+      text-align: center;
+      display: flex;
+      justify-content: space-around;
+      flex-direction: column;
+      vertical-align: middle;
+  }
+  .repeating_values-colored {
+      background-color: grey;
+      height: 400px;
+  }
+</style>
+
 </head>
 <body>
     <div class="wrapper">
@@ -43,15 +44,18 @@ let () =
     <div class="test_suites">
         <h2 style="padding: 100px 20%">We are comparing those tests</h2>
         <div class="repeating_values_wrapper">
-            <div class="repeating_value repeating_values-colored">
-                We can compare not one,
-            </div>
-            <div class="repeating_value">
-                not two,
-            </div>
-            <div class="repeating_value repeating_values-colored">
-                but three values!
-            </div>
+            <div class="repeating_value repeating_values-colored" >
+  We can compare not one,
+</div>
+
+            <div class="repeating_value" >
+  not two,
+</div>
+
+            <div class="repeating_value repeating_values-colored" >
+  but three values!
+</div>
+
         </div>
     </div>
 </body>

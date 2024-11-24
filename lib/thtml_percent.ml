926c7f08b0b7f3385679ee79d57fb7d3d8630334
0KateloK0 <katelkin2003@mail.ru>
2024-11-24 17:20:55 +0300

    Added thtml_percent bin

--- lib/thtml_percent.ml
+++ lib/thtml_percent.ml
@@ -1,69 +1,65 @@
 open Tyxml
 
-let this_title = Html.txt "Your Cool Web Page"
+let styles = [%html
+  [Html.txt {|
+    .wrapper {
+      background-color: grey;
+      padding: 100px 20% 100px 20%;
+    }
 
-let image_box = [%html
-  "<div id=image_box></div>"
+    #padding {
+      font-size: 20px;
+      color: lightblue;
+    }
+
+    body {
+      margin: 0;
+    }
+
+    .repeating_values_wrapper {
+      display: flex;
+      justify-content: space-evenly;
+    }
+
+    .repeating_value {
+      width: 100%;
+      text-align: center;
+      display: flex;
+      justify-content: space-around;
+      flex-direction: column;
+      vertical-align: middle;
+    }
+
+    .repeating_values-colored {
+      background-color: grey;
+      height: 400px;
+    }|}]
 ]
 
-let links_box = [%html {|
-  <ul class=links_bar id=links_bar>
-    <li id=home_click >My Musings</li>
-    <li id=about_click >About Me</li>
-    <li id=blog_posts_click >Blog</li>
-    <li id=hackathons_click >Hackathons</li>
-  </ul>
-|}]
-
-let common_footer = [%html {|
-  <footer id="footer_box">
-    <p>
-      This site was made with <a href=http://ocaml.org>OCaml</a> and <a href=https://www.gnu.org/software/emacs/>emacs</a>
-    </p>
-  </footer>
-|}]
-
-let home_content = [%html
-  "<div><h2>Hello Coder</h2></div>"
+let repeating_value ~is_colored ~children = [%html
+"<div class=" ["repeating_value"; if is_colored then "repeating_values-colored" else ""] ">"children"</div>"
 ]
 
-let main_payload = [%html
-  "<div id=payload>"[home_content]"</div>"
-]
-
-let common_nav = Html.nav [links_box]
-
-let content_box = [%html
-  "<div id=content_box>"[
-    common_nav;
-    main_payload;
-    common_footer;
-  ]"</div>"
-]
-
-let main_script = [%html
-  "<script src=main.js> </script>"
-]
-
-let home_page_doc = [%html
-  {|<html>
-    <head>
-     <title>|}this_title{|</title>
-     <link rel=stylesheet href="home.css" />
-    </head>
-    <body>|} [ image_box; content_box; main_script ] {|</body>
-    </html>
-|}]
-
-(** The set of pages in your website. *)
-let pages = [("index.html", home_page_doc)]
-
-(** Small code to emit all the pages. *)
-let emit_page (name, page) =
-  Printf.printf "Generating: %s\n" name ;
-  let file_handle = open_out name in
-  let fmt = Format.formatter_of_out_channel file_handle in
-  Format.fprintf fmt "%a@." (Html.pp ~indent:true ()) page;
-  close_out file_handle
-
-let () = List.iter emit_page pages
+let doc = [%html
+{|<html>|}
+  "<head>"
+      "<title>My comparison test</title>"
+      "<style>"styles"</style>"
+  "</head>"
+  {|<body>
+    <div class=wrapper>
+      <h1 style="font-size: 40px;color:aliceblue;">Cumtest <br> The future of testing</h1>
+      <p id=padding>
+        Imagine, you want to compare several templaters doing the same task.
+      </p>
+    </div>
+    <div class=test_suites>
+      <h2 style="padding: 100px 20%">We are comparing those tests</h2>
+      <div class="repeating_values_wrapper">|}[
+            repeating_value ~is_colored:true ~children:[Html.txt "We can compare not one,"];
+            repeating_value ~is_colored:false ~children:[Html.txt "not two,"];
+            repeating_value ~is_colored:true ~children:[Html.txt "but three values!"];
+    ]{|</div>
+    </div>
+  </body>
+</html>|}]
--- test/cover_tests/thtml_percent_test.ml
+++ test/cover_tests/thtml_percent_test.ml
@@ -1,1 +1,51 @@
-let () = (assert (true))
+let () =
+print_string @@ Format.asprintf "%a" (Tyxml.Html.pp ()) Thtml_percent.doc;
+(assert (
+  Format.asprintf "%a" (Tyxml.Html.pp ()) Thtml_percent.doc =
+  {|<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"><head><title>My comparison test</title><style>
+    .wrapper {
+      background-color: grey;
+      padding: 100px 20% 100px 20%;
+    }
+
+    #padding {
+      font-size: 20px;
+      color: lightblue;
+    }
+
+    body {
+      margin: 0;
+    }
+
+    .repeating_values_wrapper {
+      display: flex;
+      justify-content: space-evenly;
+    }
+
+    .repeating_value {
+      width: 100%;
+      text-align: center;
+      display: flex;
+      justify-content: space-around;
+      flex-direction: column;
+      vertical-align: middle;
+    }
+
+    .repeating_values-colored {
+      background-color: grey;
+      height: 400px;
+    }</style></head><body>
+    <div class="wrapper">
+      <h1 style="font-size: 40px;color:aliceblue;">Cumtest <br/> The future of testing</h1>
+      <p id="padding">
+        Imagine, you want to compare several templaters doing the same task.
+      </p>
+    </div>
+    <div class="test_suites">
+      <h2 style="padding: 100px 20%">We are comparing those tests</h2>
+      <div class="repeating_values_wrapper"><div class="repeating_value repeating_values-colored">We can compare not one,</div><div class="repeating_value ">not two,</div><div class="repeating_value repeating_values-colored">but three values!</div></div>
+    </div>
+
+</body></html>|}
+))
